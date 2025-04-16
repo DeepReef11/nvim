a0794e6bd15f7e2a525aa96afcbce21c91981e76
ok <ok@ok>
2025-04-16 09:57:53 -0400

    theme onedark_dark

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -10,4 +10,15 @@ return {
       require("config.colorscheme")
     end,
   },
+
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+    config = function()
+      -- load the colorscheme here
+      -- vim.cmd([[colorscheme tokyonight]])
+      vim.cmd("colorscheme " .. EcoVim.colorscheme)
+      require("config.colorscheme")
+    end,
+  }
 }
