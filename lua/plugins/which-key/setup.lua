7079bc65b1eb8ffd2fed0d7dbb5365c285d0e240
DeepReef11 <deepreef11@pm.me>
2026-02-05 19:19:25 -0500

    fix: ypp

--- lua/plugins/which-key/setup.lua
+++ lua/plugins/which-key/setup.lua
@@ -70,13 +70,9 @@ wk.add({
 wk.add({
 	{ "gs", group = "Surround", mode = { "n", "v" } },
 })
--- Yank path
+-- Yank path (registered for which-key display only, actual mappings in keymappings.lua)
 wk.add({
 	{ "yp", group = "Yank path" },
-	{ "ypp", function() local p = vim.fn.expand("%:p");   vim.fn.setreg("+", p); vim.notify(p) end, desc = "Yank absolute path" },
-	{ "ypr", function() local p = vim.fn.expand("%:.");   vim.fn.setreg("+", p); vim.notify(p) end, desc = "Yank relative path" },
-	{ "ypf", function() local p = vim.fn.expand("%:t");   vim.fn.setreg("+", p); vim.notify(p) end, desc = "Yank filename" },
-	{ "ypb", function() local p = vim.fn.expand("%:t:r"); vim.fn.setreg("+", p); vim.notify(p) end, desc = "Yank basename (no ext)" },
 })
 -- single
 wk.add({
