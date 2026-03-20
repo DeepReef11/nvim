e24e8e6979ede8a748495e5d47aa8f83a32b6497
DeepReef11 <deepreef11@pm.me>
2026-03-20 18:45:56 -0400

    feat:oil date modif

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -19,7 +19,7 @@ return {
           "icon",
           -- "permissions",
           -- "size",
-          -- "mtime",
+          "mtime",
         },
         -- Buffer-local options to use for oil buffers
         buf_options = {
